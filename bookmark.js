34c951b4e36d4d8efe8c28ba65ab09475fbd317b
Jonas Johansson <jns.johansson@gmail.com>
2020-07-18 11:19:38 +0200

    update packages and add extension support

--- bookmark.js
+++ bookmark.js
@@ -1,157 +1,158 @@
-const { shell } = require('electron').remote;
-const { URL } = require('url');
-const faviconUrl = require('favicon-url');
-const lookup = require('./lookup');
-const EventEmitter = require('event-emitter-es6');
+const { shell } = require('electron').remote
+const { URL } = require('url')
+const faviconUrl = require('favicon-url')
+const lookup = require('./lookup')
+const EventEmitter = require('event-emitter-es6')
 
-const iconPath = './assets/icons/';
+const iconPath = './assets/icons/'
 
 class Bookmark extends EventEmitter {
-  constructor(data) {
-    super();
+    constructor(data) {
+        super()
 
-    data.isMuted = data.isMuted || false;
-    data.isHidden = data.isHidden || false;
-    data.isDark = data.isDark || false;
+        data.isMuted = data.isMuted || false
+        data.isHidden = data.isHidden || false
+        data.isDark = data.isDark || false
 
-    this.handle = document.createElement('div');
-    this.handle.classList.add('bookmark');
-    this.handle.classList.toggle('is-muted', data.isMuted);
-    this.handle.classList.toggle('is-hidden', data.isHidden);
+        this.handle = document.createElement('div')
+        this.handle.classList.add('bookmark')
+        this.handle.classList.toggle('is-muted', data.isMuted)
+        this.handle.classList.toggle('is-hidden', data.isHidden)
 
-    this.view = new WebView();
-    this.view.autosize = true;
-    this.view.nodeIntegration = false;
-    this.view.allowPopups = false;
-    this.view.classList.toggle('is-dark', data.isDark);
+        this.view = new WebView()
+        // this.view = document.createElement("iframe")
+        this.view.autosize = true
+        this.view.nodeIntegration = false
+        this.view.allowPopups = true
+        this.view.classList.toggle('is-dark', data.isDark)
 
-    // https://github.com/meetfranz/franz/issues/1185
-    if (data.url.includes('whatsapp')) {
-      this.view.useragent =
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36';
-    } else {
-      // this.view.useragent = 'Chrome';
+        // https://github.com/meetfranz/franz/issues/1185
+        // if (data.url.includes('whatsapp')) {
+        //     this.view.useragent =
+        //         'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+        // } else {
+        //     this.view.useragent = 'Chrome';
+        // }
+
+        this.view.allowpopups = true
+
+        this.handleIcon = document.createElement('div')
+        this.handleIcon.classList.add('icon')
+
+        this.view.src = this.addHTTP(data.url)
+
+        this.getIcon(this.view.src)
+
+        this.handle.appendChild(this.handleIcon)
+
+        /* Listeners */
+
+        this.view.addEventListener('dom-ready', event => {
+            this.view.audioMuted = data.isMuted
+
+            if (document.documentElement.classList.contains('dark-mode') && !data.isDark) {
+                this.view.insertCSS('img, svg { filter: invert(100%); opacity: 1;  }')
+            }
+        })
+
+        this.view.addEventListener('new-window', event => {
+            event.preventDefault()
+            const url = new URL(event.url)
+            const href = url.href
+            const protocol = url.protocol
+            if (protocol === 'http:' || protocol === 'https:') {
+                console.log(href)
+                if (
+                    href.includes('accounts.google.com') ||
+                    href.includes('https://mail.google.com/mail/') ||
+                    href.includes('?authuser')
+                )
+                    this.view.src = href
+                else shell.openExternal(href)
+            }
+        })
+
+        this.view.addEventListener('page-title-updated', event => {
+            event.preventDefault()
+            const title = event.title
+
+            if (this.view.src.includes('messenger') && !title.includes('Messenger')) {
+                return
+            }
+
+            let messageCount = /\(([0-9]+)\)/.exec(title)
+            messageCount = messageCount ? Number(messageCount[1]) : 0
+
+            if (title.includes('Slack')) messageCount = title.indexOf('*') > -1 ? 1 : 0
+
+            this.handle.classList.toggle('unread', messageCount)
+            this.handle.setAttribute('data-message-count', messageCount)
+            this.emit('page-title-updated', this)
+        })
+
+        this.handle.addEventListener('click', () => {
+            this.emit('click', this)
+        })
+
+        // this.handle.addEventListener('contextmenu', () => {
+        // 	this.emit('contextmenu', this);
+        // });
     }
 
-    this.view.allowpopups = true;
-
-    this.handleIcon = document.createElement('div');
-    this.handleIcon.classList.add('icon');
-
-    this.view.src = this.addHTTP(data.url);
-
-    this.getIcon(this.view.src);
-
-    this.handle.appendChild(this.handleIcon);
-
-    /* Listeners */
-
-    this.view.addEventListener('dom-ready', event => {
-      this.view.audioMuted = data.isMuted;
-
-      if (document.documentElement.classList.contains('dark-mode') && !data.isDark) {
-        this.view.insertCSS('img, svg { filter: invert(100%); opacity: 1;  }');
-      }
-    });
-
-    this.view.addEventListener('new-window', event => {
-      event.preventDefault();
-      const url = new URL(event.url);
-      const href = url.href;
-      const protocol = url.protocol;
-      if (protocol === 'http:' || protocol === 'https:') {
-        console.log(href);
-        if (
-          href.includes('accounts.google.com') ||
-          href.includes('https://mail.google.com/mail/') ||
-          href.includes('?authuser')
-        )
-          this.view.src = href;
-        else shell.openExternal(href);
-      }
-    });
-
-    this.view.addEventListener('page-title-updated', event => {
-      event.preventDefault();
-      const title = event.title;
-
-      if (this.view.src.includes('messenger') && !title.includes('Messenger')) {
-        return;
-      }
-
-      let messageCount = /\(([0-9]+)\)/.exec(title);
-      messageCount = messageCount ? Number(messageCount[1]) : 0;
-
-      if (title.includes('Slack')) messageCount = title.indexOf('*') > -1 ? 1 : 0;
-
-      this.handle.classList.toggle('unread', messageCount);
-      this.handle.setAttribute('data-message-count', messageCount);
-      this.emit('page-title-updated', this);
-    });
-
-    this.handle.addEventListener('click', () => {
-      this.emit('click', this);
-    });
-
-    // this.handle.addEventListener('contextmenu', () => {
-    // 	this.emit('contextmenu', this);
-    // });
-  }
-
-  show() {
-    this.handle.classList.add('active');
-    this.view.classList.remove('hidden');
-  }
-
-  hide() {
-    this.handle.classList.remove('active');
-    this.view.classList.add('hidden');
-  }
-
-  reload() {
-    this.view.reload();
-  }
-
-  back() {
-    this.view.goBack();
-  }
-
-  forward() {
-    this.view.goForward();
-  }
-
-  getIcon(url) {
-    for (let entry of lookup) {
-      if (url.includes(entry[0])) {
-        this.setIcon(iconPath + entry[1]);
-        return;
-      }
+    show() {
+        this.handle.classList.add('active')
+        this.view.classList.remove('hidden')
     }
-    const host = new URL(url).host;
-    faviconUrl(host, { timeout: 2000, minBufferLength: 400 }, favicon => {
-      if (favicon !== null) {
-        this.setIcon(favicon);
-      }
-    });
-  }
 
-  setIcon(icon) {
-    if (icon.length > 8) {
-      this.handleIcon.style.backgroundImage = `url(${icon})`;
-      this.handleIcon.innerHTML = '';
-    } else {
-      this.handleIcon.innerHTML = icon;
-      this.handleIcon.style.backgroundImage = '';
+    hide() {
+        this.handle.classList.remove('active')
+        this.view.classList.add('hidden')
     }
-    this.handleIcon.setAttribute('data-icon', icon);
-  }
 
-  addHTTP(url) {
-    if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
-      url = `http://${url}`;
+    reload() {
+        this.view.reload()
     }
-    return url;
-  }
+
+    back() {
+        this.view.goBack()
+    }
+
+    forward() {
+        this.view.goForward()
+    }
+
+    getIcon(url) {
+        for (let entry of lookup) {
+            if (url.includes(entry[0])) {
+                this.setIcon(iconPath + entry[1])
+                return
+            }
+        }
+        const host = new URL(url).host
+        faviconUrl(host, { timeout: 2000, minBufferLength: 400 }, favicon => {
+            if (favicon !== null) {
+                this.setIcon(favicon)
+            }
+        })
+    }
+
+    setIcon(icon) {
+        if (icon.length > 8) {
+            this.handleIcon.style.backgroundImage = `url(${icon})`
+            this.handleIcon.innerHTML = ''
+        } else {
+            this.handleIcon.innerHTML = icon
+            this.handleIcon.style.backgroundImage = ''
+        }
+        this.handleIcon.setAttribute('data-icon', icon)
+    }
+
+    addHTTP(url) {
+        if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+            url = `http://${url}`
+        }
+        return url
+    }
 }
 
-module.exports = Bookmark;
+module.exports = Bookmark
--- index.js
+++ index.js
@@ -1,77 +1,79 @@
-'use strict';
-const { app, BrowserWindow, ipcMain } = require('electron');
-const { download } = require('electron-dl');
-const electron = require('electron');
-const tray = require('./tray');
-const menu = require('./menu');
-const config = require('./config');
+'use strict'
+const { app, session, BrowserWindow, ipcMain } = require('electron')
+const { download } = require('electron-dl')
+const electron = require('electron')
+const tray = require('./tray')
+const menu = require('./menu')
+const config = require('./config')
+const path = require('path')
 
-download.directory = app.getPath('desktop');
+download.directory = app.getPath('desktop')
 
 // https://github.com/sindresorhus/electron-context-menu
 // https://medium.com/missive-app/make-your-electron-app-dark-mode-compatible-c23dcfdd0dfa
 // https://electronjs.org/docs/tutorial/mojave-dark-mode-guide
 
-let win = null;
+let win = null
 
-app.disableHardwareAcceleration();
-// https://www.electronjs.org/docs/api/app#appallowrendererprocessreuse
-app.allowRendererProcessReuse = true;
+app.disableHardwareAcceleration()
 
-app.on('ready', () => {
-  electron.Menu.setApplicationMenu(menu);
-  createWindow();
-});
+app.on('ready', async () => {
+    electron.Menu.setApplicationMenu(menu)
+    createWindow()
+    // https://www.electronjs.org/docs/api/session#sesloadextensionpath
+    await session.defaultSession.loadExtension(path.join(__dirname, 'addons/list-layouts-for-trello/'))
+    await session.defaultSession.loadExtension(path.join(__dirname, 'addons/next-step-for-trello/'))
+})
 
 app.on('window-all-closed', () => {
-  app.quit();
-});
+    app.quit()
+})
 
 app.on('activate', () => {
-  win.show();
-});
+    win.show()
+})
 
 app.on('before-quit', () => {
-  config.set('lastWindowState', win.getBounds());
-});
+    config.set('lastWindowState', win.getBounds())
+})
 
 const createWindow = () => {
-  const lastWindowState = config.get('lastWindowState');
+    const lastWindowState = config.get('lastWindowState')
 
-  win = new BrowserWindow({
-    title: app.name,
-    x: lastWindowState.x,
-    y: lastWindowState.y,
-    width: lastWindowState.width,
-    height: lastWindowState.height,
-    minWidth: 400,
-    minHeight: 200,
-    webPreferences: {
-      nodeIntegration: true,
-      // https://electronjs.org/docs/api/webview-tag
-      // https://electronjs.org/docs/api/browser-window
-      webviewTag: true
-    }
-  });
+    win = new BrowserWindow({
+        title: app.name,
+        x: lastWindowState.x,
+        y: lastWindowState.y,
+        width: lastWindowState.width,
+        height: lastWindowState.height,
+        minWidth: 400,
+        minHeight: 200,
+        webPreferences: {
+            nodeIntegration: true,
+            // https://electronjs.org/docs/api/webview-tag
+            // https://electronjs.org/docs/api/browser-window
+            webviewTag: true
+        }
+    })
 
-  win.loadURL(`file://${__dirname}/index.html`);
+    win.loadURL(`file://${__dirname}/index.html`)
 
-  tray.create(win);
-};
+    tray.create(win)
+}
 
 // let badgeIcon = config.get('badgeIcon');
 
 ipcMain.on('page-title-updated', (events, args) => {
-  app.badgeCount = args;
-  tray.setBadge(args);
-  // if (badgeIcon !== '') {
-  // let msg = args > 0 ? badgeIcon : '';
-  // msg = "\033[" + 31 + "m" + msg + "\033[0m";
-  // tray.setTitle(args.toString());
-  // tray.setTitle(msg);
-  // }
-});
+    app.badgeCount = args
+    tray.setBadge(args)
+    // if (badgeIcon !== '') {
+    // let msg = args > 0 ? badgeIcon : '';
+    // msg = "\033[" + 31 + "m" + msg + "\033[0m";
+    // tray.setTitle(args.toString());
+    // tray.setTitle(msg);
+    // }
+})
 
 ipcMain.on('createNew', () => {
-  createWindow();
-});
+    createWindow()
+})
