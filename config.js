90e5d91a9c599d1385779bc553f059c49d31964f
Jonas Johansson <jns.johansson@gmail.com>
2020-11-15 13:29:11 +0100

    update

--- config.js
+++ config.js
@@ -1,51 +1,46 @@
-'use strict'
-const Store = require('electron-store')
+"use strict";
+const Store = require("electron-store");
 
 module.exports = new Store({
-    defaults: {
-        lastWindowState: {
-            x: 0,
-            y: 0,
-            width: 800,
-            height: 600
-        },
-        // showUnreadBadge: true,
-        // badgeIcon: '',
-        darkMode: false,
-        showHidden: false,
-        alwaysOnTop: false,
-        bookmarks: [
-            {
-                url: 'https://messenger.com/',
-                isMuted: true
-            },
-            {
-                url: 'https://gmail.com/'
-            },
-            {
-                url: 'https://calendar.google.com/'
-            },
-            {
-                url: 'https://drive.google.com/drive/'
-            },
-            {
-                url: 'https://docs.google.com/'
-            },
-            {
-                url: 'https://spreadsheets.google.com/'
-            },
-            {
-                url: 'https://trello.com/'
-            },
-            {
-                url: 'https://web.whatsapp.com/'
-            },
-            {
-                url: 'https://discordapp.com/channels/@me',
-                isMuted: true,
-                isDark: true
-            }
-        ],
-        extensions: []
-    }
-})
+  defaults: {
+    lastWindowState: {
+      x: 0,
+      y: 0,
+      width: 800,
+      height: 600,
+    },
+    // showUnreadBadge: true,
+    // badgeIcon: '',
+    darkMode: false,
+    showHidden: false,
+    alwaysOnTop: false,
+    bookmarks: [
+      {
+        url: "https://messenger.com/",
+        isMuted: true,
+      },
+      {
+        url: "https://mail.google.com/mail/u/0/",
+      },
+      {
+        url: "https://calendar.google.com/calendar/u/0/",
+      },
+      {
+        url: "https://drive.google.com/drive/",
+      },
+      // {
+      //   url: "https://docs.google.com/document/u/0/",
+      // },
+      // {
+      //   url: "https://docs.google.com/spreadsheets/u/0",
+      // },
+      {
+        url: "https://trello.com/",
+      },
+      {
+        url: "https://web.whatsapp.com/",
+      },
+    ],
+    extensions: [],
+  },
+});
